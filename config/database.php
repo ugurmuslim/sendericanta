ee8367664d7839c6308ad41db4bac854ed08be53
ugur muslim <ugur.muslim@gmail.com>
2018-11-14 21:34:13 +0300

    Sarebu Advertisemnet

--- config/database.php
+++ config/database.php
@@ -99,7 +99,7 @@ return [
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', 'foobared'),
+            'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
