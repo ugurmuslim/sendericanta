51a9ae1272f858bd48ffdf99e37aaeba91daf45f
ugur muslim <ugur.muslim@gmail.com>
2018-11-23 11:31:27 +0300

    new improvements

--- Modules/Shop/Resources/views/partials/_slider.blade.php
+++ Modules/Shop/Resources/views/partials/_slider.blade.php
@@ -8,33 +8,41 @@
               <span class="ltext-101 cl2 respon2">
                 <div class="row">
                   <div class="col-md-6">
-                  <img src="{{asset('modules/shop/images/logos/northface2.png')}}" alt="">
+                    <a href="{{route('brands.products',["brand_slug" => 'north-face',"category_slug" => "none"])}}">
+                  <img src="{{asset('modules/shop/images/logos/northface2.png')}}" class="slider_logos" alt="">
+                </a>
                 </div>
                 <div class="col-md-6">
-                  <img src="{{asset('modules/shop/images/logos/vans-logo1.jpg')}}" alt="">
+                  <a href="{{route('brands.products',["brand_slug" => 'vans',"category_slug" => "none"])}}">
+                  <img src="{{asset('modules/shop/images/logos/vans-logo1.jpg')}}" class="slider_logos" alt="">
+                </a>
                 </div>
                 </div>
-                <div class="row mt-5">
+                <div class="row">
                   <div class="col-md-6">
-                    <img src="{{asset('modules/shop/images/logos/National-Geographic-2.png')}}" alt="">
+                  <a href="{{route('brands.products',["brand_slug" => 'national-geographic',"category_slug" => "none"])}}">
+                    <img src="{{asset('modules/shop/images/logos/national4.jpg')}}" class="slider_logos second_row" alt="">
+                    </a>
                 </div>
                 <div class="col-md-6">
-                  <img src="{{asset('modules/shop/images/logos/eastpak1.png')}}" alt="">
+                  <a href="{{route('brands.products',["brand_slug" => 'eastpak',"category_slug" => "none"])}}">
+                  <img src="{{asset('modules/shop/images/logos/eastpak1.png')}}" class="slider_logos second_row" alt="">
+                </a>
                 </div>
 
                 </div>
               </span>
             </div>
 
-            <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
+        {{--    <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
               <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
                 EN YENİ MODELLER
               </h2>
             </div>
-
-            <div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
+--}}
+            <div class="layer-slick1 animated visible-false mt-4" data-appear="zoomIn" data-delay="1600">
               <a href="#butix_products" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-                ŞİMDİ ALIŞVERİŞE BAŞLAYIN
+                DERİ ÜRÜNLERİMİZE GÖZ ATIN
               </a>
             </div>
           </div>
@@ -48,31 +56,39 @@
               <span class="ltext-101 cl2 respon2">
                 <div class="row">
                   <div class="col-md-6">
-                  <img src="{{asset('modules/shop/images/logos/columbia2.png')}}" alt="">
+                    <a href="{{route('brands.products',["brand_slug" => 'columbia',"category_slug" => "none"])}}">
+                  <img src="{{asset('modules/shop/images/logos/columbia2.png')}}" class="slider_logos" alt="">
+                </a>
                 </div>
                 <div class="col-md-6">
-                  <img src="{{asset('modules/shop/images/logos/DELSEY2.png')}}" alt="">
+                  <a href="{{route('brands.products',["brand_slug" => 'delsey',"category_slug" => "none"])}}">
+                  <img src="{{asset('modules/shop/images/logos/DELSEY2.png')}}" class="slider_logos" alt="">
+                </a>
                 </div>
                 </div>
-                <div class="row mt-5">
+                <div class="row ">
                   <div class="col-md-6">
-                    <img src="{{asset('modules/shop/images/logos/fouvor2.png')}}" alt="">
+                    <a href="{{route('brands.products',["brand_slug" => 'fouvor',"category_slug" => "none"])}}">
+                    <img src="{{asset('modules/shop/images/logos/fouvor2.png')}}" class="slider_logos second_row" alt="">
+                  </a>
                 </div>
                 <div class="col-md-6">
-                  <img src="{{asset('modules/shop/images/logos/Hummel2.png')}}" alt="">
+                  <a href="{{route('brands.products',["brand_slug" => 'hummel',"category_slug" => "none"])}}">
+                  <img src="{{asset('modules/shop/images/logos/Hummel2.png')}}" class="slider_logos second_row" alt="">
+                </a>
                 </div>
 
                 </div>
               </span>
             </div>
 
-            <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
+        {{--    <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
               <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
                 YÜKSEK KALİTE
               </h2>
             </div>
-
-            <div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
+--}}
+            <div class="layer-slick1 animated visible-false mt-4" data-appear="slideInUp" data-delay="1600">
               <a href="#butix_products" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
                 ŞİMDİ ALIŞVERİŞE BAŞLAYIN
               </a>
