d136e7f6a5ae6219d24667820099a42a3b3a81d5
ugur muslim <ugur.muslim@gmail.com>
2018-11-26 22:55:16 +0300

    some fixing

--- public/modules/shop/js/slick-custom.js
+++ public/modules/shop/js/slick-custom.js
@@ -125,7 +125,7 @@
                 {
                   breakpoint: 576,
                   settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 1
                   }
                 }
