c651e79d4907dcf957457aeb2c2e53dbda565ebd
ugur muslim <ugur.muslim@gmail.com>
2018-12-11 20:21:06 +0300

    senderi canta index

--- Modules/Shop/Resources/views/search-results.blade.php
+++ Modules/Shop/Resources/views/search-results.blade.php
@@ -33,28 +33,16 @@
 					<div class="block2">
 						<div class="block2-pic hov-img0">
 							<img src="{{asset('images/products/' . $product->images()->mainImage(1)->name)}}" style="width:255px; height:315px;" alt="{{$product->slug}}">
-
-							<a href="{{route('shop.product-detail',$product->slug)}}" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-								{{__('views.shop.products_quickview')}}
-							</a>
 						</div>
 
 						<div class="block2-txt flex-w flex-t p-t-14">
 							<div class="block2-txt-child1 flex-col-l ">
 								<a href="{{route('product.shop-detail',$product->slug)}}" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-
 								{{$product->name}}
 								</a>
 								<span class="stext-105 cl3">
 								<span class="simge-tl">&#8378;</span> {{$product->price}}
 								</span>
-							</div>
-
-							<div class="block2-txt-child2 flex-r p-t-3">
-								<a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-									<img class="icon-heart1 dis-block trans-04" src="modules/shop/images/icons/icon-heart-01.png" alt="ICON">
-									<img class="icon-heart2 dis-block trans-04 ab-t-l" src="modules/shop/images/icons/icon-heart-02.png" alt="ICON">
-								</a>
 							</div>
 						</div>
 					</div>
